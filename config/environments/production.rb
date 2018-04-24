424fcde6278a2aca71d16c20ee5e80178e333eb1
Masahiro Okada <masahiro.okada@modernworks.jp>
2018-04-24 20:00:41 +0900

    change aws config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,12 +83,12 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_protocol: :https,
-    bucket: ENV['S3_BUCKET_NAME'],
-    s3_region: ENV['AWS_REGION'],
-    s3_host_name: ENV['AWS_HOST'],
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    s3_region: ENV.fetch('AWS_REGION'),
+    s3_host_name: ENV.fetch('AWS_HOST'),
     s3_credentials: {
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
     }
   }
 end
